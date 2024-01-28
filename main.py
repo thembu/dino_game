a4d3ce6f65b2cef2002a35a46848d3591c4209ba
thembu <rakhivhanithembu@gmail.com>
2024-01-28 23:33:37 +0200

    added ai to it

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+import neat
 import pygame
 import os
 import random
@@ -20,8 +23,8 @@ Jumping = pygame.image.load(os.path.join('assets/Dino', 'DinoJump.png'))
 BG = pygame.image.load(os.path.join('assets/Other', 'Track.png'))
 
 SMALL_CACTUS = [pygame.image.load(os.path.join("assets/Cactus", "SmallCactus1.png")),
-               pygame.image.load(os.path.join("assets/Cactus", "SmallCactus2.png")),
-               pygame.image.load(os.path.join("assets/Cactus", "SmallCactus3.png"))]
+                pygame.image.load(os.path.join("assets/Cactus", "SmallCactus2.png")),
+                pygame.image.load(os.path.join("assets/Cactus", "SmallCactus3.png"))]
 LARGE_CACTUS = [pygame.image.load(os.path.join("assets/Cactus", "LargeCactus1.png")),
                 pygame.image.load(os.path.join("assets/Cactus", "LargeCactus2.png")),
                 pygame.image.load(os.path.join("assets/Cactus", "LargeCactus3.png"))]
@@ -91,7 +94,6 @@ class Obstacles:
         screen.blit(self.image[self.type], self.rect)
 
 
-
 class SmallCactus(Obstacles):
     def __init__(self, image, number_of_cactus):
         super().__init__(image, number_of_cactus)
@@ -103,13 +105,35 @@ class LargeCactus(Obstacles):
         super().__init__(image, number_of_cactus)
         self.rect.y = 300
 
+
 def remove(index):
     dinosaurs.pop(index)
-def main():
-    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, dinosaurs
+    ge.pop(index)
+    nets.pop(index)
+
+
+def distance(pos_a, pos_b):
+    dx = pos_a[0] - pos_b[0]
+    dy = pos_a[1] - pos_b[1]
+
+    return math.sqrt(dx ** 2 + dy ** 2)
+
+
+def eval_genomes(genomes, config):
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, dinosaurs, ge, nets
     run = True
     clock = pygame.time.Clock()
-    dinosaurs = [Dinasour()]
+    dinosaurs = []
+    ge = []
+    nets = []
+
+    for genome_id, genome in genomes:
+        dinosaurs.append(Dinasour())
+        ge.append(genome)
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        genome.fitness = 0
+
     obstacles = []
     x_pos_bg = 0
     y_pos_bg = 380
@@ -147,17 +171,16 @@ def main():
             dinosaur.update()
             dinosaur.draw(SCREEN)
 
-
         if len(dinosaurs) == 0:
             break
 
         if len(obstacles) == 0:
             rand_int = random.randint(0, 1)
             if rand_int == 0:
-                obstacles.append(SmallCactus(SMALL_CACTUS , random.randint(0,2)))
+                obstacles.append(SmallCactus(SMALL_CACTUS, random.randint(0, 2)))
 
             elif rand_int == 1:
-                obstacles.append(LargeCactus(LARGE_CACTUS , random.randint(0,2)))
+                obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
 
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
@@ -165,12 +188,15 @@ def main():
 
             for i, dinosaur in enumerate(dinosaurs):
                 if dinosaur.rect.colliderect(obstacle.rect):
+                    ge[i].fitness -= 1
                     remove(i)
 
-        keys = pygame.key.get_pressed()
-
         for i, dinosaur in enumerate(dinosaurs):
-            if keys[pygame.K_SPACE]:
+            output = nets[i].activate((dinosaur.rect.y,
+                                       distance((dinosaur.rect.x, dinosaur.rect.y),
+                                                obstacle.rect.midtop)))
+
+            if output[0] > 0.5 and dinosaur.rect.y == dinosaur.Y_POS:
                 dinosaur.dino_jump = True
                 dinosaur.dino_run = False
 
@@ -180,4 +206,20 @@ def main():
         pygame.display.update()
 
 
-main()
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    pop = neat.Population(config)
+    pop.run(eval_genomes, 50)
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
